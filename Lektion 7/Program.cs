529dc4615b2c9777793dbc24bc165df13a23c734
Felix Magnusson <felix.magnusson01@gmail.com>
2023-09-11 10:28:34 +0200

    Ändring gjord

--- Lektion 7/Program.cs
+++ Lektion 7/Program.cs
@@ -6,7 +6,8 @@
         {
             Console.WriteLine("Hello, Felix!");
             Console.WriteLine("Detta är version 2 ");
-            
+            Console.WriteLine("Detta är en ändring");
+            Console.WriteLine("Detta är också enn ändring");
         }
     }
 }
