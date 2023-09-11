a33c8065a43e7c12c25ba62a8d438c6d9f21d913
Felix Magnusson <felix.magnusson01@gmail.com>
2023-09-11 09:58:08 +0200

    Add project files.

---
+++ Lektion 7/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lektion_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Felix!");
+            
+        }
+    }
+}
